776a2751f9775e11b907d9fae933cfa91d69c300
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-08-29 22:33:47 -0400

    logging items from data and piecing to parts of html

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 var APIKey = "c96ff37ce7e15d7cf6a6ddc995e12f66";
-var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
-var cityName;
 
 var searchButton = document.getElementById('search-button');
 var searchField = document.getElementById('text-field');
+var currentSection = document.querySelector('.current');
 
 function searchForCity() {
+    var cityName = searchField.value.trim();
+
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
+
+            for (var i = 0; i < data.length; i++) {
+                var mainTemp = document.createElement('h2');
+                mainTemp.classList.add('main-temp');
+                mainTemp.textContent = data[i].main.temp;
+                currentSection.appendChild(mainTemp);
+            };
         })
 };
 
