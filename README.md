d9f38c2c0c8bdff4b5a343db40e39d2b3ba9854b
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-08-30 19:53:20 -0400

    parameters temp, wind, humidity, and date added to current day upon search and dates on future 5 days

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # weather-via-alexw
 06 Server-Side APIs: Weather Dashboard HW
+
+##Checklist:
+
+Current day: City Name
+             Date
+             Icon repping conditions
+             Temp
+             Humidity
+             *UV Index
--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 var APIKey = "c96ff37ce7e15d7cf6a6ddc995e12f66";
 
+var today = moment();
+var currentDate = document.getElementById('current-date');
+var tomorrow = document.getElementById('day1');
+var day2 = document.getElementById('day2');
+var day3 = document.getElementById('day3');
+var day4 = document.getElementById('day4');
+var day5 = document.getElementById('day5');
+var currentDateAtSearch = today.format('dddd - MMM D, YYYY');
+
 var searchButton = document.getElementById('search-button');
 var searchField = document.getElementById('text-field');
+
 var currentSection = document.querySelector('.current');
+var currentTemp = document.getElementById('current-temp');
+var currentWind = document.getElementById('current-wind');
+var currentHumidity = document.getElementById('current-humidity');
+var currentUvi = document.getElementById('current-uvi');
+
+var forecastTemp = document.querySelector('.forecast-temp');
 
 function searchForCity() {
-    var cityName = searchField.value.trim();
-
-    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    var cityName = searchField.value.trim(); // text field in html's value
+    // formats temperature to F°
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + APIKey;
+    var requestUrlForecast = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + cityName + "&cnt=5&units=imperial&appid=" + APIKey;
 
     fetch(requestUrl)
         .then(function (response) {
@@ -15,14 +32,17 @@ function searchForCity() {
         })
         .then(function (data) {
             console.log(data);
-
-            for (var i = 0; i < data.length; i++) {
-                var mainTemp = document.createElement('h2');
-                mainTemp.classList.add('main-temp');
-                mainTemp.textContent = data[i].main.temp;
-                currentSection.appendChild(mainTemp);
-            };
-        })
+            currentTemp.textContent = parseInt(data.main.temp) + "°"; // logs temp as forced whole num
+            currentWind.textContent = "Wind: " + parseInt(data.wind.speed) + "mph";
+            currentHumidity.textContent = "Humidity: " + data.main.humidity + "%";
+            currentUvi.textContent = "UVi: 0.55";
+            currentDate.textContent = currentDateAtSearch;
+            tomorrow.textContent = today.add(1, 'd').format('ddd, MMM D'); // today +1
+            day2.textContent = today.add(1, 'd').format('ddd, MMM D'); // plus a day count followed
+            day3.textContent = today.add(1, 'd').format('ddd, MMM D');
+            day4.textContent = today.add(1, 'd').format('ddd, MMM D');
+            day5.textContent = today.add(1, 'd').format('ddd, MMM D');
+        });
 };
 
 searchButton.addEventListener('click', searchForCity);
