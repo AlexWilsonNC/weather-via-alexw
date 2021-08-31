56e45031b68345314d8e984074267a7e07f2076e
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-08-30 22:54:15 -0400

    finished wind, humidity, temps, name, current day, 5 day

--- script.js
+++ script.js
@@ -18,12 +18,14 @@ var currentTemp = document.getElementById('current-temp');
 var currentWind = document.getElementById('current-wind');
 var currentHumidity = document.getElementById('current-humidity');
 var currentUvi = document.getElementById('current-uvi');
+var iconCurrent = document.getElementById('iconCurrent');
+
+var cityList = [];
 
 function searchForCity() {
     var cityName = searchField.value.charAt(0).toUpperCase() + searchField.value.slice(1).toLowerCase();
     // forcing any value entered, first char capitalized since I'm displaying their searched city on the page afterwards
     var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + APIKey;
-    var requestUrluvi = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=" + APIKey;
     var requestUrlForecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + APIKey;
 
     fetch(requestUrl)
@@ -35,22 +37,21 @@ function searchForCity() {
             currentTemp.textContent = parseInt(data.main.temp) + "°"; // logs temp as forced whole num
             currentWind.textContent = "Wind: " + parseInt(data.wind.speed) + "mph";
             currentHumidity.textContent = "Humidity: " + data.main.humidity + "%";
-            currentUvi.textContent = "UVi: 0.55"; ///////////////////////////////////////////////////////////////
+            currentUvi.textContent = "UVi: 0.55"; // example, replace
+            var weatherIcon = document.getElementById('iconCurrent');
+            var currentIcon = data.weather[0].icon;
+            weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${currentIcon}.png`);
+            weatherIcon.setAttribute('alt', data.weather[0].icon)
             currentDate.textContent = currentDateAtSearch;
             tomorrow.textContent = today.add(1, 'd').format('ddd, MMM D'); // today +1
             day2.textContent = today.add(1, 'd').format('ddd, MMM D'); // plus a day each count followed
             day3.textContent = today.add(1, 'd').format('ddd, MMM D');
             day4.textContent = today.add(1, 'd').format('ddd, MMM D');
             day5.textContent = today.add(1, 'd').format('ddd, MMM D');
-            document.getElementById('current-header').textContent = cityName;
-        });
-
-    fetch(requestUrluvi)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log("uv", data);
+            var header = document.getElementById('current-header').textContent = cityName;
+            // if (data.weather[0].description === "overcast clouds") {
+            //     document.getElementById('cloud-current').classList.remove('hide');
+            // }
         });
 
     fetch(requestUrlForecast)
